d33cb3b43230632c88d8aa8ce34b5a880064dffb
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-04 00:32:36 -0400

    Update settings.py

--- skin_diagnostic/settings.py
+++ skin_diagnostic/settings.py
@@ -32,6 +32,8 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 #]
 
 ALLOWED_HOSTS = [h.strip() for h in os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",") if h.strip()]
+print("RAILWAY_HOST:", os.environ.get("RAILWAY_HOST", "⚠️ Not Found"))
+
 # Tu peux ajouter RAILWAY_HOST si tu veux être plus souple :
 railway_host = os.environ.get("RAILWAY_HOST")
 if railway_host and railway_host not in ALLOWED_HOSTS:
