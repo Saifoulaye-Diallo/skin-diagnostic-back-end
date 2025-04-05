9a5cde1fbdfc58ecc24018c73db48c7a7c66c3c4
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-05 00:09:08 -0400

    Update settings.py

--- skin_diagnostic/settings.py
+++ skin_diagnostic/settings.py
@@ -77,6 +77,15 @@ MIDDLEWARE = [
 ]
 ROOT_URLCONF = 'skin_diagnostic.urls'
 
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
