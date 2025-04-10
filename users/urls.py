077b9f3a13f1b6580d328ad53caca0584132e7f5
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-10 18:59:27 -0400

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView, ProfileView
+from .views import RegisterView, ProfileView, UpdateProfileView, ChangePasswordView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/update/', UpdateProfileView.as_view(), name='update_profile'),
+    path('profile/password/', ChangePasswordView.as_view(), name='change_password'),
 ]
