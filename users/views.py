2287cb78bdfd9c0480aac9f66d795c8dca2c5861
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-10 18:45:52 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -19,3 +19,10 @@ class ProfileView(APIView):
         user = request.user
         serializer = UserProfileSerializer(user)
         return Response(serializer.data)
+        
+  def patch(self, request): 
+        serializer = UserProfileSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
