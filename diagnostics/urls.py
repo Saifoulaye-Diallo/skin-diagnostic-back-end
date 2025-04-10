fe20db7767cff2d5d20ddd40fec6076a35d8be2b
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-10 01:38:12 -0400

    Update urls.py

--- diagnostics/urls.py
+++ diagnostics/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import DiagnosticView, ImageDiagnosticListView
+from .views import DiagnosticView, ImageDiagnosticListView,UpdateDiagnosticView,DeleteDiagnosticView
 
 urlpatterns = [
     path('diagnostic/', DiagnosticView.as_view(), name='diagnostic'),
     path('images/', ImageDiagnosticListView.as_view(), name='image-list'),
+
+    # 🔁 Ajout de update et delete
+    path('diagnostic/update/<int:id>/', UpdateDiagnosticView.as_view(), name='update-diagnostic'),
+    path('diagnostic/delete/<int:id>/', DeleteDiagnosticView.as_view(), name='delete-diagnostic'),
 ]
